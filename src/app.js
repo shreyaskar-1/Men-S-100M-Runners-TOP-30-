f3545fa25bc0d78c02797b2bb0f06cf0363b945f
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-06-20 11:40:53 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -17,6 +17,32 @@ app.post("/mens", async (req, res) => {
     }
 });
 
+// reading data
+app.get("/mens", async (req, res) => {
+    try {
+        const getMens = await Mensranking.find({});
+        res.send(getMens); 
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+});
+
+// reading data individually
+app.get("/mens/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const getMen = await Mensranking.findById({_id});
+        res.send(getMen); 
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+});
+
+// updating the data
+
+
+
+
 app.listen(port, () => {
     console.log(`Connection Is Successful At ${port}`);
 });
